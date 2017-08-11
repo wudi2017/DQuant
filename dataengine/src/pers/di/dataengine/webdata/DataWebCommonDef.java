11e6e88b34d22bb195975c37147fae9825db8ea7
wudi <wudi19870903@163.com>
2017-08-11 21:13:17 +0800

    x

--- dataengine/src/pers/di/dataengine/webdata/DataWebCommonDef.java
+++ dataengine/src/pers/di/dataengine/webdata/DataWebCommonDef.java
@@ -46,8 +46,10 @@ public class DataWebCommonDef {
 	 */
 	public static class DayKData implements Comparable
 	{
-		// 2015-09-18
+		// 2015-09-18 or null
 		public String date;
+		// 13:25:20 or null
+		public String time;
 		public float open;
 		public float close;
 		public float low;
@@ -57,7 +59,40 @@ public class DataWebCommonDef {
 		public int compareTo(Object arg0) {
 			// TODO Auto-generated method stub
 			DayKData sdto = (DayKData)arg0;
-		    return this.date.compareTo(sdto.date);
+			int iRet = 0;
+			// date compare
+			if(null != this.date && null != sdto.date)
+			{
+				iRet = this.date.compareTo(sdto.date);
+				if(0 == iRet)
+				{
+					// time compare
+					if(null != this.time && null != sdto.time)
+					{
+						return this.time.compareTo(sdto.time);
+					}
+					else if(null != this.time && null == sdto.time)
+					{
+						return 1;
+					}
+					else 
+					{
+						return -1;
+					}
+				}
+				else
+				{
+					return iRet;
+				}
+			}
+			else if(null != this.date && null == sdto.date)
+			{
+				return 1;
+			}
+			else 
+			{
+				return -1;
+			}
 		}
 	}
 	
