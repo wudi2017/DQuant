968e08a237d197ed6e601f94c9ef37998ce15549
wudi <wudi19870903@163.com>
2017-11-06 11:43:13 +0800

    add test strategy select

--- quantplatform/src/pers/di/quantplatform_test/TestQuantStrategy.java
+++ quantplatform/src/pers/di/quantplatform_test/TestQuantStrategy.java
@@ -1,7 +1,6 @@
 package pers.di.quantplatform_test;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 import pers.di.account.*;
 import pers.di.account.common.*;
@@ -14,8 +13,9 @@ import pers.di.quantplatform.*;
 public class TestQuantStrategy {
 	
 	public static String s_accountDataRoot = CSystem.getRWRoot() + "\\account";
-	public static float s_transactionCostsRatioBuy = 0.02f;
-	public static float s_transactionCostsRatioSell = 0.05f;
+	public static float s_transactionCostsRatioBuy = 0.0016f;
+	public static float s_transactionCostsRatioSell = 0.0f;
+	
 	public static class MockMarketOpe extends IMarketOpe
 	{
 		@Override
@@ -32,10 +32,45 @@ public class TestQuantStrategy {
 		}
 	}
 	
+	/*
+	 * SelectResultWrapper类，用于选股优先级排序
+	 */
+	static private class SelectResult {
+		
+		// 优先级从大到小排序
+		static public class SelectResultCompare implements Comparator 
+		{
+			public int compare(Object object1, Object object2) {
+				SelectResult c1 = (SelectResult)object1;
+				SelectResult c2 = (SelectResult)object2;
+				int iCmp = Float.compare(c1.fPriority, c2.fPriority);
+				if(iCmp > 0) 
+					return -1;
+				else if(iCmp < 0) 
+					return 1;
+				else
+					return 0;
+			}
+		}
+		
+		public SelectResult(){
+			stockID = "";
+			fPriority = 0.0f;
+		}
+		public String stockID;
+		public float fPriority;
+	}
+	
 	public static class TestStrategy extends QuantStrategy
 	{
+		public TestStrategy()
+		{
+			m_seletctID = new ArrayList<String>();
+		}
+		
 		@Override
 		public void onInit(QuantContext ctx) {
+			
 		}
 	
 		@Override
@@ -49,12 +84,63 @@ public class TestQuantStrategy {
 		@Override
 		public void onDayFinish(QuantContext ctx) {
 			CLog.output("TEST", "TestStrategy.onDayFinish %s %s", ctx.date(), ctx.time());
+			
+			m_seletctID.clear();
+			
+			// select strategy
+			List<SelectResult> cSelectResultList = new ArrayList<SelectResult>();
 			for(int i=0; i<ctx.pool().size(); i++)
 			{
 				DAStock cDAStock = ctx.pool().get(i);
+				DAKLines cDAKLines = cDAStock.dayKLines();
+				int iSize = cDAKLines.size();
+				if(iSize > 4)
+				{
+					KLine cStockDayCur = cDAKLines.get(iSize-1);
+					KLine cStockDayBefore1 = cDAKLines.get(iSize-2);
+					KLine cStockDayBefore2 = cDAKLines.get(iSize-3);
+
+					if(cStockDayCur.close < cStockDayCur.open 
+							&& cStockDayCur.close < cStockDayBefore1.close
+							&& cStockDayBefore1.close < cStockDayBefore1.open
+							&& cStockDayBefore1.close < cStockDayBefore2.close
+							)
+					{
+						SelectResult cSelectResult = new SelectResult();
+						cSelectResult.stockID = cDAStock.ID();
+						cSelectResult.fPriority = cStockDayBefore2.close - cStockDayCur.close;
+						cSelectResultList.add(cSelectResult);
+					}
+				}
+			}
+			Collections.sort(cSelectResultList, new SelectResult.SelectResultCompare());
+			
+			int maxSelectCnt = 3;
+			int iSelectCount = cSelectResultList.size();
+			int iAddCount = iSelectCount>maxSelectCnt?maxSelectCnt:iSelectCount;
+			for(int i=0; i<iAddCount; i++)
+			{
+				m_seletctID.add(cSelectResultList.get(i).stockID);
 			}
 			
+			// output Selected log
+			String logStr = "";
+			logStr += String.format("Selected (%d) [ ", iAddCount);
+			if(iAddCount == 0) logStr += "null ";
+			for(int i=0; i< iAddCount; i++)
+			{
+				String stockId = m_seletctID.get(i);
+				logStr += String.format("%s ", stockId);
+				if (i >= 7 && m_seletctID.size()-1 > 8) {
+					logStr += String.format("... ", stockId);
+					break;
+				}
+			}
+			logStr += String.format("]");
+			CLog.output("TEST", "%s\n", logStr);
 		}
+		
+		private List<String> m_seletctID;
 	}
 	
 	@CTest.test
