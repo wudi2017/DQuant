6f795f3c020bfb0f492078339ab5a9ee95806b91
Your Name <you@example.com>
2017-11-08 00:44:53 +0800

    x

--- common/src/pers/di/common_test/TestCUtilsMath.java
+++ common/src/pers/di/common_test/TestCUtilsMath.java
@@ -1,11 +1,24 @@
 package pers.di.common_test;
 
-import pers.di.common.CLog;
-import pers.di.common.CUtilsMath;
+import pers.di.common.*;
 
 public class TestCUtilsMath {
-	public static void main(String[] args) {
-		CLog.output("TEST", "TestBUtilsDateTime begin\n");
+	
+	@CTest.test
+	public static void test_math45()
+	{
+		float pi=3.1415926f;
+		int iTest = 0;
+		for(int i=0; i<10000*10000; i++)
+		{
+			iTest += Math.round(pi);
+		}
+		CLog.output("TEST", "iTest %d",iTest);
+	}
+	
+	@CTest.test
+	public static void test_ooo()
+	{
 		
 		CLog.output("TEST", "saveNDecimal %f\n", CUtilsMath.saveNDecimalIgnore(2.199f, 1));
 		CLog.output("TEST", "saveNDecimal %f\n", CUtilsMath.saveNDecimalIgnore(2.199f, 2));
@@ -23,7 +36,9 @@ public class TestCUtilsMath {
 		CLog.output("TEST", "saveNDecimal45 %.3f = 30.16?\n", CUtilsMath.saveNDecimal(27.42f*1.1f, 2));
 		CLog.output("TEST", "saveNDecimal45 %.3f = 30.18?\n", CUtilsMath.saveNDecimal(30.16f*1.1f, 2));
 		
-
-		CLog.output("TEST", "TestBUtilsDateTime end\n");
+	}
+	public static void main(String[] args) {
+		CTest.ADD_TEST(TestCUtilsMath.class);
+		CTest.RUN_ALL_TESTS();
 	}
 }
