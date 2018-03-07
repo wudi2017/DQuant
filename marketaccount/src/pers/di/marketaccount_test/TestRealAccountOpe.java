d3b2a7675277fe709f5177c138e41cd649588161
wudi <wudi19870903@163.com>
2018-03-07 21:22:33 +0800

    upgrade THSAPI

--- marketaccount/src/pers/di/marketaccount_test/TestRealAccountOpe.java
+++ marketaccount/src/pers/di/marketaccount_test/TestRealAccountOpe.java
@@ -12,10 +12,99 @@ import pers.di.common.*;
 import pers.di.marketaccount.mock.MockAccountOpe;
 import pers.di.marketaccount.real.RealAccountOpe;
 
+import pers.di.thsapi.*;
+
 public class TestRealAccountOpe {
 	
 	public static String s_accountDataRoot = CSystem.getRWRoot() + "\\account";
+	
+	
+	@CTest.test
+	public static void testTHSApi()
+	{
+		Formatter fmt = new Formatter(System.out);
+		CLog.output("TEST", "### main begin\n");
+		
+		// 初始化测试
+		{
+			int iInitRet = THSApi.initialize();
+			CLog.output("TEST", "CATHSAccount.initialize err(%d)", iInitRet);
+		}
+		        
+		// 可用资金测试
+		{
+			THSApi.ObjectContainer<Float> container = new THSApi.ObjectContainer<Float>();
+	        int ret = THSApi.getAvailableMoney(container);
+	        CLog.output("TEST", "CATHSAccount.getAvailableMoney err(%d) AvailableMoney(%.2f)", ret, container.get());
+		}
 
+		// 总资产测试
+		{
+			THSApi.ObjectContainer<Float> container = new THSApi.ObjectContainer<Float>();
+	        int ret =  THSApi.getTotalAssets(container);
+	        CLog.output("TEST", "CATHSAccount.getTotalAssets err(%d) TotalAssets(%.2f)", ret, container.get());
+		}
+
+		// 所有股票总市值测试
+		{
+			THSApi.ObjectContainer<Float> container = new THSApi.ObjectContainer<Float>();
+	        int ret =  THSApi.getAllStockMarketValue(container);
+	        CLog.output("TEST", "CATHSAccount.getAllStockMarketValue err(%d) AllStockMarketValue(%.2f)", ret, container.get());
+		}
+
+		// 持股列表测试
+		{
+			List<THSApi.HoldStock> container = new ArrayList<THSApi.HoldStock>();
+	        int ret = THSApi.getHoldStockList(container);
+	        CLog.output("TEST", "CATHSAccount.getHoldStockList err(%d) resultList size(%d)", ret, container.size());
+	        for(int i=0;i<container.size();i++)
+	        {
+	        	THSApi.HoldStock cHoldStock = container.get(i);
+	        	CLog.output("TEST", "    {%s %d %d %.3f %.3f %.3f}", cHoldStock.stockID, cHoldStock.totalAmount, cHoldStock.availableAmount,
+	        			cHoldStock.refProfitLoss, cHoldStock.refPrimeCostPrice, cHoldStock.curPrice);
+	        }
+		}
+				
+		// 委托列表测试
+		{
+			List<THSApi.CommissionOrder> container = new ArrayList<THSApi.CommissionOrder>();
+	        int ret = THSApi.getCommissionOrderList(container);
+	        CLog.output("TEST", "CATHSAccount.getCommissionOrderList err(%d) resultList size(%d)", ret, container.size());
+	        for(int i=0;i<container.size();i++)
+	        {
+	        	THSApi.CommissionOrder cCommissionOrder = container.get(i);
+	        	CLog.output("TEST", "    {%s %s %s %d %.3f %d %.3f}", cCommissionOrder.time, cCommissionOrder.stockID, cCommissionOrder.tranAct.toString(), 
+	        			cCommissionOrder.commissionAmount, cCommissionOrder.commissionPrice,
+	        			cCommissionOrder.dealAmount, cCommissionOrder.dealPrice);
+	        }
+		}
+				
+		// 成交列表测试
+		{
+			List<THSApi.DealOrder> container = new ArrayList<THSApi.DealOrder>();
+	        int ret = THSApi.getDealOrderList(container);
+	        CLog.output("TEST", "CATHSAccount.getDealOrderList err(%d) resultList size(%d)", ret, container.size());
+	        for(int i=0;i<container.size();i++)
+	        {
+	        	THSApi.DealOrder cDealOrder = container.get(i);
+	        	CLog.output("TEST", "    {%s %s %s %d %.3f}", cDealOrder.time, cDealOrder.stockID, cDealOrder.tranAct.toString(), 
+	        			cDealOrder.dealAmount, cDealOrder.dealPrice);
+	        }
+		}
+		            
+		// 买入委托测试
+		{
+	        // int iBuyRet = THSApi.buyStock("601988", 100, 0.07f);
+	        // CLog.output("TEST", "CATHSAccount.buyStock err(%d)", iBuyRet);
+		}
+        
+		// 卖出委托测试
+		{
+	        // int iSellRet = THSApi.sellStock("601988", 100, 190.7f);
+	        // CLog.output("TEST", "CATHSAccount.sellStock err(%d)", iSellRet);
+		}
+	}
+	
 	@CTest.test
 	public static void testRealAccountOpe()
 	{
@@ -35,11 +124,11 @@ public class TestRealAccountOpe {
 //				"601988", 100, 0.99f);
 //		CTest.EXPECT_LONG_EQ(0, iSell);
 		
-//		CObjectContainer<Float> ctnAvailableMoney = new CObjectContainer<Float>();
+//		CTHSApi.ObjectContainer<Float> ctnAvailableMoney = new CTHSApi.ObjectContainer<Float>();
 //		int iAvailableMoney = cRealAccountOpe.getAvailableMoney(ctnAvailableMoney);
 //		CTest.EXPECT_LONG_EQ(0, iAvailableMoney);
 		
-//		CObjectContainer<Float> ctnMoney = new CObjectContainer<Float>();
+//		CTHSApi.ObjectContainer<Float> ctnMoney = new CTHSApi.ObjectContainer<Float>();
 //		int iMoney = cRealAccountOpe.getMoney(ctnMoney);
 //		CTest.EXPECT_LONG_EQ(0, iMoney);
 //		CTest.EXPECT_TRUE(ctnMoney.get() > 100.0f);
@@ -75,7 +164,7 @@ public class TestRealAccountOpe {
 		// TODO Auto-generated method stub
 		CSystem.start();
 		CTest.ADD_TEST(TestRealAccountOpe.class);
-		CTest.RUN_ALL_TESTS("");
+		CTest.RUN_ALL_TESTS("TestRealAccountOpe.testTHSApi");
 		CSystem.stop();
 	}
 }
